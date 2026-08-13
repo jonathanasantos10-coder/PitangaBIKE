36535426cd3c42c021c63f40b688b2bc3482495f
jonathan <jonathan.a.santos10@aluno.senai.br>
2026-08-12 21:42:05 -0300

    adicionado o componento Home para ser a página inicial da página estática e adicionado os routes que serão usados no routerLink.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Cadastro } from './components/cadastro/cadastro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: Home
+    },
+    {
+        path: 'cadastro',
+        component: Cadastro
+    },
+
+];
